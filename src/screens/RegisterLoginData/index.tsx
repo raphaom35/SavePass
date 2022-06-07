0ddb2a1ff58640c4c73f0dc3ecaf119b0c32b886
Raphael <rapha35om.rm@gmail.com>
2022-06-07 11:21:28 -0300

    first commit

---
+++ src/screens/RegisterLoginData/index.tsx
@@ -0,0 +1,111 @@
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Alert, KeyboardAvoidingView, Platform } from "react-native";
+import { useForm } from "react-hook-form";
+import { RFValue } from "react-native-responsive-fontsize";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import uuid from "react-native-uuid";
+
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Form/Input";
+import { Button } from "../../components/Form/Button";
+
+import { Container, Form } from "./styles";
+
+interface FormData {
+  service_name: string;
+  email: string;
+  password: string;
+}
+
+const schema = Yup.object().shape({
+  service_name: Yup.string().required("Nome do serviço é obrigatório!"),
+  email: Yup.string()
+    .email("Não é um email válido")
+    .required("Email é obrigatório!"),
+  password: Yup.string().required("Senha é obrigatória!"),
+});
+
+export function RegisterLoginData() {
+  const { navigate } = useNavigation();
+  const {
+    reset,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  async function handleRegister(formData: FormData) {
+    const newLoginData = {
+      id: String(uuid.v4()),
+      ...formData,
+    };
+    try {
+      const dataKey = "@savepass:logins";
+      const data = await AsyncStorage.getItem(dataKey);
+      const currentData = data ? JSON.parse(data) : [];
+
+      const newData = [...currentData, newLoginData];
+
+      await AsyncStorage.setItem(dataKey, JSON.stringify(newData));
+      reset();
+      navigate("Home");
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Não foi possível salvar");
+    }
+  }
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+      enabled
+    >
+      <Header />
+      <Container>
+        <Form>
+          <Input
+            testID="service-name-input"
+            title="Nome do serviço"
+            name="service_name"
+            error={errors.service_name && errors.service_name.message}
+            control={control}
+            autoCapitalize="sentences"
+            autoCorrect
+          />
+          <Input
+            testID="email-input"
+            title="E-mail"
+            name="email"
+            error={errors.email && errors.email.message}
+            control={control}
+            autoCorrect={false}
+            autoCapitalize="none"
+            keyboardType="email-address"
+          />
+          <Input
+            testID="password-input"
+            title="Senha"
+            name="password"
+            error={errors.password && errors.password.message}
+            control={control}
+            secureTextEntry
+          />
+
+          <Button
+            style={{
+              marginTop: RFValue(8),
+            }}
+            title="Salvar"
+            onPress={handleSubmit(handleRegister)}
+          />
+        </Form>
+      </Container>
+    </KeyboardAvoidingView>
+  );
+}
